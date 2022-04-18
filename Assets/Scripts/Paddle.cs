e47642b05e8b1511d7c21cc5619b5528ed929f57
Riccardo Angeli <103458110+riky2001onxbox@users.noreply.github.com>
2022-04-18 17:39:34 +0200

    Add files via upload

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    public void Move(Vector2 _direction)
+    {
+        transform.Translate(_direction * MoveSpeed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * MoveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * MoveSpeed * Time.deltaTime);
+        }
+    }
+}
